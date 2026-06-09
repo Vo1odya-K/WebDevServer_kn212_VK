235a0d50098f51bbf8f2db8b3bb756795af393d8
Vova <volodymyr.kalamazh@oa.edu.ua>
2026-06-09 10:10:14 +0000

    Complete Lab13

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\Blog\Admin\CategoryController;
 use App\Http\Controllers\Api\Blog\Admin\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DiggingDeeperController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -27,4 +28,12 @@ Route::group($groupData, function () {
     Route::apiResource('posts', PostController::class)
         ->except(['show'])                               //не робити маршрут для метода show
         ->names('blog.admin.posts');
+});
+
+Route::prefix('digging_deeper')->group(function () {
+    Route::get('process-video', [DiggingDeeperController::class, 'processVideo'])
+        ->name('digging_deeper.processVideo');
+
+    Route::get('prepare-catalog', [DiggingDeeperController::class, 'prepareCatalog'])
+        ->name('digging_deeper.prepareCatalog');
 });
